ca3157a0491042b0155b2629410b189f4ab3c8c8
Shintaro Inagaki <shinagaki@gmail.com>
2024-02-11 10:57:31 +0900

    boilerplateに合わせて修正

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,7 +4,7 @@ const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
 
 module.exports = {
-  '*': ['secretlint --maskSecrets'],
-  '*.{js,cjs,mjs,json,ts,tsx,css}': ['prettier --write'],
+  '*': ['secretlint'],
+  '*.{js,cjs,mjs,json,ts,tsx,css,scss}': ['prettier --write'],
   '*.{ts,tsx}': ['bash -c tsc --noEmit', buildEslintCommand, 'markuplint'],
 };
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:size-auto lg:bg-none dark:from-black dark:via-black">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://vercel.com"
             rel="noopener noreferrer"
             target="_blank"
           >
@@ -34,7 +34,7 @@ export default function Home() {
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://nextjs.org/docs"
           rel="noopener noreferrer"
           target="_blank"
         >
@@ -51,7 +51,7 @@ export default function Home() {
 
         <a
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://nextjs.org/learn"
           rel="noopener noreferrer"
           target="_blank"
         >
@@ -68,7 +68,7 @@ export default function Home() {
 
         <a
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://vercel.com/templates"
           rel="noopener noreferrer"
           target="_blank"
         >
@@ -83,7 +83,7 @@ export default function Home() {
 
         <a
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://vercel.com/new"
           rel="noopener noreferrer"
           target="_blank"
         >
